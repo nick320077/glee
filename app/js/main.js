cda8b446f30e79bdcbc95059994c14402bddff91
nick320077 <kobets32@gmail.com>
2020-12-14 14:26:58 +0200

    added header and footer

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,10 @@
-$(function(){
- 
+$(function () {
+  var mixer = mixitup(".product__items");
+  $(".top-slider__inner").slick({
+    arrows: false,
+    dots: true,
+    fade: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
 });
