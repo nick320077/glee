87ea77b73c97b717e78ab6c3b7388d618a9740cd
nick320077 <kobets32@gmail.com>
2020-12-22 19:11:53 +0200

    added product-list

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,28 @@
 $(function () {
+  $(".shop__recent-rate").rateYo({
+    starWidth: "13px",
+    normalFill: "#d6d6d6",
+    ratedFill: "#ffcc00",
+    readOnly: true,
+  });
+  $(".shop__price-input").ionRangeSlider({
+    type: "double",
+    min: 0,
+    max: 1000,
+    from: 400,
+    to: 600,
+    prefix: "$",
+    onStart: function (data) {
+      $(".shop__price-from").text(data.from.toFixed(2));
+      $(".shop__price-to").text(data.to.toFixed(2));
+    },
+
+    onChange: function (data) {
+      $(".shop__price-from").text(data.from.toFixed(2));
+      $(".shop__price-to").text(data.to.toFixed(2));
+    },
+  });
+
   var mixer = mixitup(".product__items");
   var mixer = mixitup(".product-new__items");
   $(".top-slider__inner").slick({
