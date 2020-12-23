b9877a753866b669f69386cc48c67d453451afd6
nick320077 <kobets32@gmail.com>
2020-12-23 15:23:38 +0200

    added product-list-2

--- app/js/main.js
+++ app/js/main.js
@@ -1,11 +1,19 @@
 $(function () {
-  $(".shop__recent-rate").rateYo({
+  $(".shop__recent-rate, .shop-page__recent-rate").rateYo({
     starWidth: "13px",
     normalFill: "#d6d6d6",
     ratedFill: "#ffcc00",
     readOnly: true,
   });
-  $(".shop__price-input").ionRangeSlider({
+
+  $(".shop-page__product-rate").rateYo({
+    starWidth: "18px",
+    normalFill: "#d6d6d6",
+    ratedFill: "#ffcc00",
+    readOnly: true,
+    spacing: "10px",
+  });
+  $(".shop__price-input, .shop-page__price-input").ionRangeSlider({
     type: "double",
     min: 0,
     max: 1000,
@@ -13,13 +21,13 @@ $(function () {
     to: 600,
     prefix: "$",
     onStart: function (data) {
-      $(".shop__price-from").text(data.from.toFixed(2));
-      $(".shop__price-to").text(data.to.toFixed(2));
+      $(".shop__price-from, .shop-page__price-from").text(data.from.toFixed(2));
+      $(".shop__price-to, .shop-page__price-to").text(data.to.toFixed(2));
     },
 
     onChange: function (data) {
-      $(".shop__price-from").text(data.from.toFixed(2));
-      $(".shop__price-to").text(data.to.toFixed(2));
+      $(".shop__price-from, .shop-page__price-from").text(data.from.toFixed(2));
+      $(".shop__price-to, .shop-page__price-to").text(data.to.toFixed(2));
     },
   });
 
