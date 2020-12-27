e70c1d714a65a8d07680f87a79c7f4edc098e9f0
nick320077 <kobets32@gmail.com>
2020-12-27 15:57:29 +0200

    added product-details

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,38 @@
 $(function () {
+  $(".product-related__slider-items").slick({
+    slidesToShow: 4,
+  });
+
+  $(".product-tabs__tab").on("click", function (e) {
+    e.preventDefault();
+    $(".product-tabs__tab").removeClass("product-tabs__tab--active");
+    $(this).addClass("product-tabs__tab--active");
+
+    $(".product-tabs__content-item").removeClass(
+      "product-tabs__content-item--active"
+    );
+    $($(this).attr("href")).addClass("product-tabs__content-item--active");
+  });
+
+  $(".product-details__num").styler();
+
+  $(".product-details__slide-thumbs").slick({
+    asNavFor: ".product-details__slide-big",
+    focusOnSelect: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    vertical: true,
+    draggable: false,
+    arrows: false,
+  });
+
+  $(".product-details__slide-big").slick({
+    asNavFor: ".product-details__slide-thumbs",
+    draggable: false,
+    fade: true,
+    arrows: false,
+  });
+
   $(".shop__recent-rate, .shop-page__recent-rate").rateYo({
     starWidth: "13px",
     normalFill: "#d6d6d6",
@@ -6,7 +40,7 @@ $(function () {
     readOnly: true,
   });
 
-  $(".shop-page__product-rate").rateYo({
+  $(".shop-page__product-rate, .product-details__rate").rateYo({
     starWidth: "18px",
     normalFill: "#d6d6d6",
     ratedFill: "#ffcc00",
@@ -33,6 +67,7 @@ $(function () {
 
   var mixer = mixitup(".product__items");
   var mixer = mixitup(".product-new__items");
+
   $(".top-slider__inner").slick({
     arrows: false,
     dots: true,
